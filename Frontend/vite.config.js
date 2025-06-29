250222e670aee1da05329988c129a2915db0116d
Adarsh Chandra <adarshworkplace@gmail.com>
2025-06-29 20:42:36 +0530

    Link changes to socket

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const BASE_URL = mode === "development" ? "http://localhost:4002" : "";
+  // const BASE_URL = mode === "development" ? "http://localhost:4002" : "";
   return {
     plugins: [react()],
     server: {
       port: 3001,
       proxy: {
         "/api": {
-          target: BASE_URL,
+          target: "http://localhost:4002",
           changeOrigin: true,
         },
       },
